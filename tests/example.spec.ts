3b6631c0fe848c67210bfc0917c7db61c53ee046
Williamho4 <willyho_@hotmail.se>
2025-08-29 22:56:37 +0200

    refaktorera-tester

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,10 @@
 import { test, expect, Page } from '@playwright/test'
 
+type Movie = {
+  Title: string
+  Poster?: string
+}
+
 async function mockMovieApi(page: Page, title?: string, poster?: string) {
   await page.route('http://www.omdbapi.com/?i*', async (route) => {
     await route.fulfill({
@@ -13,7 +18,22 @@ async function mockMovieApi(page: Page, title?: string, poster?: string) {
   })
 }
 
+async function searchForMovieWithMockData(page: Page, movie: Movie) {
+  await mockMovieApi(page, movie.Title, movie.Poster)
+  await page.getByTestId('movie-search-input').fill(movie.Title)
+}
+
 test.describe('Search for movie', () => {
+  const movieWithPoster = {
+    Title: 'Cars',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTg5NzY0MzA2MV5BMl5BanBnXkFtZTYwNDc3NTc2._V1_SX300.jpg',
+  }
+
+  const movieWithoutPoster = {
+    Title: 'Cars',
+  }
+
   test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173/')
   })
@@ -30,31 +50,24 @@ test.describe('Search for movie', () => {
   })
 
   test('shows movies poster', async ({ page }) => {
-    const movie = {
-      Title: 'Cars',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMTg5NzY0MzA2MV5BMl5BanBnXkFtZTYwNDc3NTc2._V1_SX300.jpg',
-    }
-
-    await mockMovieApi(page, movie.Title, movie.Poster)
-
-    await page.getByTestId('movie-search-input').fill('cars')
+    await searchForMovieWithMockData(page, movieWithPoster)
     await expect(page.getByTestId('home-page-movie-img')).toHaveAttribute(
       'src',
-      movie.Poster
+      movieWithPoster.Poster
     )
   })
 
   test('shows no movie poster text', async ({ page }) => {
-    const movie = {
-      Title: 'Cars',
-    }
-
-    await mockMovieApi(page, movie.Title)
-
-    await page.getByTestId('movie-search-input').fill('cars')
+    await searchForMovieWithMockData(page, movieWithoutPoster)
     await expect(page.getByTestId('home-page-no-poster-text')).toHaveText(
       /No poster/
     )
+  })
+
+  test('see more navigates to correct path', async ({ page }) => {
+    await searchForMovieWithMockData(page, movieWithoutPoster)
+
+    await page.getByTestId('see-more-btn').click()
+    await expect(page).toHaveURL(new RegExp('/movie-details/cars', 'i'))
   })
 })
